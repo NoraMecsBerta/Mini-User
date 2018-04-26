168a5b5efed121187143d08bad458aa1fceb55eb
Nora Mecs-Berta <ms.noraberta@gmail.com>
2018-04-26 22:54:48 +0200

    spring boot app was chosen and added as maven project

---
+++ src/main/java/probation/miniuser/MiniUserApp.java
@@ -0,0 +1,14 @@
+package probation.miniuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUserApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniUserApp.class, args);
+    }
+}
+
+
