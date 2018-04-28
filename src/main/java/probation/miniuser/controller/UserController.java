0ebed063148f795700db7722cbb101407f0a0f75
Nora Mecs-Berta <ms.noraberta@gmail.com>
2018-04-28 23:52:47 +0200

    send data with ajax and save new User entity

--- src/main/java/probation/miniuser/controller/UserController.java
+++ src/main/java/probation/miniuser/controller/UserController.java
@@ -1,11 +1,23 @@
 package probation.miniuser.controller;
 
+import org.json.JSONObject;
+import org.json.JSONString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import probation.miniuser.model.User;
 import probation.miniuser.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.Response;
+
+import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Controller
 public class UserController {
@@ -20,4 +32,13 @@ public class UserController {
     }
 
 
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public ResponseEntity create(@RequestBody Map<String, String> rb) {
+        String userName = rb.get("name");
+        String email = rb.get("email");
+        //missing error handling when not managed to persist the data
+        userService.addUser(new User(userName, email));
+        return new ResponseEntity(HttpStatus.OK);
+
+    }
 }
--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,50 +1,43 @@
-function showRegistrationForm() {
-    var registrationContainer = document.getElementById("registrationField");
 
-    var createform = document.createElement('form'); // Create New Element Form
-    createform.setAttribute("action", ""); // Setting Action Attribute on Form
-    createform.setAttribute("method", "post"); // Setting Method Attribute on Form
+$('button').on("click", function(e){
+    addUser();
+});
 
-    registrationContainer.appendChild(createform);
-    var heading = document.createElement('h2'); // Heading of Form
-    heading.innerHTML = "Add new user here ";
-    createform.appendChild(heading);
-    var line = document.createElement('hr'); // Giving Horizontal Row After Heading
-    createform.appendChild(line);
-    var linebreak = document.createElement('br');
-    createform.appendChild(linebreak);
-    var namelabel = document.createElement('label'); // Create Label for Name Field
-    namelabel.innerHTML = "Your Name : "; // Set Field Labels
-    createform.appendChild(namelabel);
-    var inputelement = document.createElement('input'); // Create Input Field for Name
-    inputelement.setAttribute("type", "text");
-    inputelement.setAttribute("name", "dname");
-    createform.appendChild(inputelement);
-    var linebreak = document.createElement('br');
-    createform.appendChild(linebreak);
-    var usernamelabel = document.createElement('label'); // Create Label for Address Field
-    usernamelabel.innerHTML = "User Name : "; // Set Field Labels
-    createform.appendChild(usernamelabel);
-    var inputelement = document.createElement('input'); // Create Input Field for Name
-    inputelement.setAttribute("type", "text");
-    inputelement.setAttribute("name", "usrname");
-    createform.appendChild(inputelement);
-    var linebreak = document.createElement('br');
-    createform.appendChild(linebreak);
-    var pswdlabel = document.createElement('label'); // Create Label for Password Field
-    pswdlabel.innerHTML = "Password : "; // Set Field Labels
-    createform.appendChild(pswdlabel);
-    var inputelement = document.createElement('input'); // Create Input Field for Password
-    inputelement.setAttribute("type", "password");
-    inputelement.setAttribute("name", "psswd");
-    createform.appendChild(inputelement);
-    var linebreak = document.createElement('br');
-    createform.appendChild(linebreak);
-    var doblabel = document.createElement('label'); // Create Label for date of birth
-    doblabel.innerHTML = "Your Date of Birth : "; // Set Field Labels
-    createform.appendChild(doblabel);
-    var inputelement = document.createElement('input'); // Create Input Field for date of birth
-    inputelement.setAttribute("type", "text");
-    inputelement.setAttribute("name", "dob");
-    createform.appendChild(inputelement);
+
+function addUser() {
+
+    if ($('#userName').val() !== '' || $('#email').val() !== '') {
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            url: "/addUser",
+            //dataType: "json",
+            data: formToJSON(),
+            /*complete: function() { that worked anyway*/
+            success: function () {
+                alert("success");
+                clearInputField();
+            },
+            error: function () {
+                alert("not managed")
+            }
+        });
+    } else {
+        alert("must fill both field!")
+    }
+}
+
+
+function formToJSON() {
+    var json = {
+        "name": $('#userName').val(),
+        "email": $('#email').val(),
+    }
+    return JSON.stringify(json);
+}
+
+
+function clearInputField() {
+    $('#email').val('');
+    $('#userName').val('');
 }
